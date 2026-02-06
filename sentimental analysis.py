f20e3ce8f6c5f843ad7e648a30ca610193a39284
Samar Rai <rsamar918@gmail.com>
2026-02-06 10:24:14 +0530

    Add files via upload

---
+++ sentimental analysis.py
@@ -0,0 +1,35 @@
+# %% [markdown]
+# SENTIMENTAL ANALYSIS
+# 
+
+# %%
+#this is the code for sentimental analysis for twitter 
+#using twitter api for realtime analysis (tweepy)
+
+# %%
+#importing the necesssary libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import random
+from sklearn.model_selection  import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+
+
+# %%
+#importing the dataset 
+df=pd.read_csv(r"C:\Users\ASUS\Desktop\training datasets\twitter_training.csv\twitter_training.csv")
+
+# %%
+df.tail(5)
+
+# %%
+df.info()
+
+# %%
+df.isnull().sum()
+
+
